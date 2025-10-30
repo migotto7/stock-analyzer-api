c70428225b3573b13feb0a48bc018019ed51bf25
migotto7 <elthon.migotto7@gmail.com>
2025-10-30 14:57:49 -0300

    fix: update app to delete auth route

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import stockRoutes from "./routes/stockRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -12,6 +11,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", stockRoutes);
-app.use("/api/auth", authRoutes);
 
 export default app;
